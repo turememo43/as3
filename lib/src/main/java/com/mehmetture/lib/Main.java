6090b7c3664ef38a6409f8accef19d4a741998eb
turememo43 <ture.memo.7@gmail.com>
2024-07-10 18:04:00 +0300

    variables

---
+++ lib/src/main/java/com/mehmetture/lib/Main.java
@@ -0,0 +1,24 @@
+package com.mehmetture.lib;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        double age = 20;
+
+        System.out.println( age * 15 );
+        System.out.println( age / 3 );
+
+        double pi = 3.14;
+        double r = 7;
+
+        double alan = pi*r*r;
+        double hacim = (4.0/3)*pi*r*r*r;
+
+        System.out.println("ALAN:" + alan);
+
+        System.out.println("HACİM:" + hacim);
+
+
+    }
+}
